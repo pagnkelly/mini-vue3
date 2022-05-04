ea48c43d8fa683703854e28d03e73a7bd527d328
pagnkelly <632228198@qq.com>
2022-05-04 21:22:38 +0800

    feat: element

--- example/helloworld/app.js
+++ example/helloworld/app.js
@@ -1,7 +1,13 @@
 import { h } from '../../lib/guide-mini-vue.esm.js'
 export const App = {
   render() {
-    return h('div', 'hi, ' + this.msg)
+    return h('div', {
+      id: 'root',
+      class: ['red', 'hard']
+    },
+    // 'hi, ' + this.msg
+    [h('p', { class: 'red' }, 'hi'), h('p', { class: 'blue' }, 'mini-vue')]
+    )
   },
   setup() {
     return {
--- src/runtime-core/renderer.ts
+++ src/runtime-core/renderer.ts
@@ -1,13 +1,45 @@
+import { isObject } from "../shared/index";
 import { createComponentInstance, setupComponent } from "./components";
 
 export function render(vnode, rootContainer) {
   patch(vnode, rootContainer)
 }
 
-function patch(vnode, rootContainer) {
+function patch(vnode, contariner) {
   // TODO 
-  // processElement()
-  processComponent(vnode, rootContainer);
+  if (typeof vnode.type === 'string') {
+    processElement(vnode, contariner)
+  } else if (isObject(vnode.type)) {
+    processComponent(vnode, contariner);
+  }
+}
+
+function processElement(vnode, contariner) {
+  mountElement(vnode, contariner)
+}
+
+function mountElement(vnode: any, contariner: any) {
+  const el = document.createElement(vnode.type)
+
+  const { children, props } = vnode
+  if (typeof children === 'string') {
+    el.textContent = children
+  } else if (Array.isArray(children)) {
+    mountChildren(children, el)
+  }
+
+  for (const key in props) {
+    const val = props[key]
+    el.setAttribute(key, val)
+  }
+
+  contariner.append(el)
+}
+
+function mountChildren(children, container) {
+  children.forEach(v => {
+    patch(v, container)
+  })
 }
 
 function processComponent(vnode, container) {
@@ -24,4 +56,5 @@ function setupRenderEffect(instance, container) {
   const subTree = instance.render()
   patch(subTree, container)
 }
+
 
