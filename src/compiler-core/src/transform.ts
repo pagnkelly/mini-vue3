1f283608463ac62b0de46cd495eea9525a2bb82e
pagnkelly <632228198@qq.com>
2022-05-19 17:47:29 +0800

    feat: gen string

--- src/compiler-core/src/transform.ts
+++ src/compiler-core/src/transform.ts
@@ -1,11 +1,15 @@
 import { NodeTypes } from "./ast"
 
-export function transform (root, options) {
+export function transform (root, options = {}) {
   const context = createTransformContext(root, options)
   // 1 遍历
   traverseNode(root, context)
-}
 
+  createRootCodegen(root);
+}
+function createRootCodegen(root: any) {
+  root.codegenNode = root.children[0];
+}
 function traverseNode(node: any, context) {
   const nodeTransforms = context.nodeTransforms
   for (let i = 0; i < nodeTransforms.length; i++) {
