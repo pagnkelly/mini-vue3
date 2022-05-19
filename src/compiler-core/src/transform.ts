1fdc98dcfc97a1c8cb48f3d23af0994a440e62a5
pagnkelly <632228198@qq.com>
2022-05-19 16:50:49 +0800

    feat: transform

---
+++ src/compiler-core/src/transform.ts
@@ -0,0 +1,37 @@
+import { NodeTypes } from "./ast"
+
+export function transform (root, options) {
+  const context = createTransformContext(root, options)
+  // 1 遍历
+  traverseNode(root, context)
+}
+
+function traverseNode(node: any, context) {
+  const nodeTransforms = context.nodeTransforms
+  for (let i = 0; i < nodeTransforms.length; i++) {
+    const transform = nodeTransforms[i]
+    transform(node, context)
+  }
+
+  traverseChildren(node, context)
+  
+}
+
+function traverseChildren (node: any, context) {
+  const children = node.children
+  if (children) {
+    for (let i = 0; i < children.length; i++) {
+      const node = children[i]
+      traverseNode(node, context)
+    }
+  }
+}
+
+function createTransformContext(root: any, options: any) {
+  const context = {
+    node: root,
+    nodeTransforms: options.nodeTransforms || []
+  }
+
+  return context
+}
